76cdea84d7ca90762db2a323d50f6bf2c14b9c25
nathanmiller759 <nathanmiller759@gmail.com>
2025-04-12 17:54:06 -0400

    forgot the main.

---
+++ ConsoleApplication1C++/Main.cpp
@@ -0,0 +1,72 @@
+
+#include "FileManagement.h"
+#include <iostream>
+#include<string>
+#include "Map.h";
+#include<map>
+#include <Windows.h>
+#include<vector>
+#include <sstream>
+
+using std::cout;
+using std::endl;
+using std::string;
+using std::cin;
+using std::vector;
+using std::ofstream;
+using std::map;
+using std::multimap;
+using std::pair;
+ void print(multimap<string, int>& words);
+int main()
+{
+	FileManagement fileManage;
+	Map* mapFile = new Map();
+	string inpFileDirectory;
+	string outpFileDirectory;
+	string temFileDirectory;
+
+	//grab input directory from the user.
+	cout << "Please enter the directory that holds the input files: ";
+	cin >> inpFileDirectory;
+	fileManage.setInputFileDirectory(inpFileDirectory);
+	//grab the short name for the file.
+	fileManage.parseShortFileName(inpFileDirectory);
+	
+	//grab the output directory from the user.
+	cout << "Please enter the directory that holds the output files: ";
+	cin >> outpFileDirectory;
+	fileManage.setOutputFileDirectory(outpFileDirectory);
+	//grab the temporary directory from the user.
+	cout << "Please enter the directory that holds the temp files: ";
+	cin >> temFileDirectory;
+	fileManage.setTempFileDirectory(temFileDirectory);
+
+	string lineOfText;
+	while (lineOfText != "eof"){
+		//reads one line of text.
+		lineOfText = fileManage.readOneLineOfText(inpFileDirectory);
+		//adds to the map that holds <filename,lineoftext> and then creates the map for <word,1>
+		mapFile->mapText(fileManage.getShortFileName(),lineOfText);
+	}
+	
+	//just a print method for me to test the parsing.
+	print(*mapFile->getMap());
+}
+
+ void print(multimap<string, int> &words) {
+
+ofstream parsedOutput;
+if (!parsedOutput.is_open()) {
+	parsedOutput.open("C:\\Users\\Nate\\Documents\\shakespeare\\shakespeare\\Test.txt");
+}
+	for (const auto& pair : words) {
+		parsedOutput << pair.first << ":" << pair.second << std::endl;
+	}
+	Sleep(50);
+}
+
+
+
+
+
