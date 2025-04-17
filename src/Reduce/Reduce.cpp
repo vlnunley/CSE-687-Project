703232d80b822a92a1ed794bb84e063095958bab
ahamburglar <imchangliuu@gmail.com>
2025-04-16 21:46:30 -0700

    implement reduce function

--- src/Reduce/Reduce.cpp
+++ src/Reduce/Reduce.cpp
@@ -1,1 +1,16 @@
-// TODO
+#include "Reduce.h"
+#include<string>
+
+Reduce::Reduce() {
+
+}
+
+void Reduce::reduce(const std::string& key, const std::vector<int>& values) {
+	int sum = std::accumulate(values.begin(), values.end(), 0);
+	export(key, sum);
+}
+
+void Reduce::export(const std::string& key, const int value) {
+	// TODO - write result out to the output directory
+	// TODO - write SUCCESS to the output directory
+}
