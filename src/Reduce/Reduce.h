f2fab36d8765c08ff7b1cd2e7f9b9f4f9f44c583
ahamburglar <imchangliuu@gmail.com>
2025-04-18 19:33:07 -0700

    update constructor to get file management when initialized

--- src/Reduce/Reduce.h
+++ src/Reduce/Reduce.h
@@ -1,11 +1,17 @@
 #pragma once
 #include<string>
+#include "../FileManagement/FileManagement.h"
 
 class Reduce
 {
 public:
+    Reduce(FileManagement& fileManager);
+    ~Reduce();
+
     void reduce(const std::string& key, const std::vector<int>& values);
 
 private:
     void export(const std::string& key, const int value);
+
+    FileManagement& fileManagement;
 };
